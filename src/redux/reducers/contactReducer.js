e3e4fe2fd80630fa948fb2cff55c86ae3f5f5320
Deep Vasan <deepvasan10@gmail.com>
2022-04-13 22:13:27 +0530

    Added Delete and Update actions in reducer

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -20,6 +20,20 @@ const contactReducer = (state =initialState,action)=>{
         case "ADD_CONTACT":
             state = [...state,action.payload];
             return state;
+        case "DELETE_CONTACT":
+            const contactFilter = state.filter((contact) =>
+                contact.id === action.payload ? null : contact
+            );
+            state = contactFilter;
+            return state;
+        case "UPDATE_CONTACT":
+            const contactUpdate = state.filter((contact) =>
+                contact.id === action.payload.id
+                ? Object.assign(contact, action.payload)
+                : contact
+            );
+            state = contactUpdate;
+            return state;
         default:
             return state;
     }
