f327d91f5acb412cb3bb40c5c1fd3aefdf4c423f
Deep Vasan <deepvasan10@gmail.com>
2022-04-14 16:27:20 +0530

    Added fields for rollNo and Status of study in database

--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -1,31 +1,42 @@
-// import { getDefaultNormalizer } from "@testing-library/react";
-
+// Initialized the array to store data
 const initialState=[
     {
         id:0,
         name:"Ram",
-        number:8976543330,
+        phone:8976543330,
         email: 'ram@ramayana.com',
+        rollno: "B20Ms111",
+        status: "Studying",
     },
     {
         id:1,
         name:"Laxman",
-        number:8976543300,
+        phone:8976543300,
         email:'laxman@ramayana.com',
+        rollno: "B20MX111",
+        status: "Passout",
     },
 ];
 
+// Reducer component to perform the necessary actions
 export const contactReducer = (state =initialState,action)=>{
+
     switch(action.type){
+
+        // If action is Add contact
         case "ADD_CONTACT":
             state = [...state,action.payload];
             return state;
+
+        // If action is Delete contact
         case "DELETE_CONTACT":
             const contactFilter = state.filter((contact) =>
                 contact.id === action.payload ? null : contact
             );
             state = contactFilter;
             return state;
+
+        // If action is Update contact
         case "UPDATE_CONTACT":
             const contactUpdate = state.filter((contact) =>
                 contact.id === action.payload.id
@@ -34,6 +45,7 @@ export const contactReducer = (state =initialState,action)=>{
             );
             state = contactUpdate;
             return state;
+
         default:
             return state;
     }
